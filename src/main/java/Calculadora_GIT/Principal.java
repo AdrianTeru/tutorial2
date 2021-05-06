91e8d813aa0ef021fe5e203ea1185dfad618fb2c
Adrián Teruel Reina <adrianteru2001@gmail.com>
2021-05-06 23:18:00 +0200

    Añadida Función Módulo División

--- src/main/java/Calculadora_GIT/Principal.java
+++ src/main/java/Calculadora_GIT/Principal.java
@@ -32,10 +32,11 @@ public class Principal {
             case 2: System.out.println( " la resta es "+ casio.resta() ); break;
             case 3: System.out.println(" la multipilcacion es "+ casio.multiplicacion()); break;
             case 4: System.out.println(" La division es " + casio.division()); break;
+            case 5: System.out.print(" El modulo es "+ casio.modulo()); break;
             default: System.out.println(" Debes elegir una opción de 1 a 5");
             }
     
-        } while ( operacion != 5 );
+        } while ( operacion != 6 );
         
         System.out.println(" Gracias por usar la calculadora...hasta luegooooorrr");
         
